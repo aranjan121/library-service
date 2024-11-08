d61a80e5aac98d25d79fb29a2f64e01d33284040
aranjan121 <ashish.ranjan121@gmail.com>
2024-11-08 23:20:33 +0530

    code change

---
+++ src/main/java/com/test/library/LibraryServiceApplication.java
@@ -0,0 +1,14 @@
+package com.test.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryServiceApplication.class, args);
+		System.out.println("LibraryServiceApplication ........");
+	}
+
+}
